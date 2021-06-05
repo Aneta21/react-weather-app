637de029136aa286382d9ef2531b29e0963baf19
Aneta Stepankova <74676721+Aneta21@users.noreply.github.com>
2021-06-05 13:06:11 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Coded by Aneta and open-sourced on{" "}
-        <a
-          href="https://github.com/Aneta21/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="contrainer">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by Aneta and open-sourced on{" "}
+          <a
+            href="https://github.com/Aneta21/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
