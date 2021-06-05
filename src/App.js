6ca74cd1643b59eff8cd881a71852a58347705aa
Aneta Stepankova <74676721+Aneta21@users.noreply.github.com>
2021-06-05 11:36:16 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by Aneta and open-sourced on GitHub.
+        <a
+          href="https://github.com/Aneta21/react-weather-app"
+          target="_blank"
+        ></a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
